f85b314a5888bb9f8a11ec1d635b85124cca6a3e
rohit parte <rohitparte606@gmail.com>
2024-06-14 19:19:48 +0530

    first commit

--- App.js
+++ App.js
@@ -1,13 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import * as Location from 'expo-location';
+import * as TaskManager from "expo-task-manager";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+TaskManager.defineTask('test-app-location', ({ data, error }) => {
+  console.log('Hello - task received execution');
+  if (error) {
+    // Error occurred - check `error.message` for more details.
+    console.error('Unable to start location task.');
+    console.error(error);
+    return;
+  }
+
+  if (data) {
+    console.log(Date.now());
+    console.log(data);
+  }
+}
+);
+
+export default class App extends Component {
+  async startTracking() {
+    console.log('Attempting to start tracking.');
+    console.log(TaskManager.isTaskDefined('test-app-location'));
+    Location.startLocationUpdatesAsync('test-app-location', {
+      accuracy: Location.Accuracy.BestForNavigation,
+      showsBackgroundLocationIndicator: true,
+      deferredUpdatesInterval: 1000,
+      timeInterval: 1000,
+      foregroundService: { notificationTitle: 'GPS', notificationBody: 'Tracking in progress', notificationColor: '#029a9a' }
+    }).then(() => {
+      console.log('Location tracking - Background task');
+    }).catch((err) => {
+      console.error('Unable to start tracking')
+      console.error(err);
+    });
+
+  }
+
+  async componentDidMount() {
+    await Location.requestForegroundPermissionsAsync()
+    await Location.requestBackgroundPermissionsAsync()
+
+    Location.hasStartedLocationUpdatesAsync('test-app-location').then((result) => {
+      if (result) {
+        Location.stopLocationUpdatesAsync('test-app-location').then(() => {
+          console.log('Location updates stopped');
+        });
+      }
+    })
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <TouchableOpacity
+          style={{ backgroundColor: '#004470', borderRadius: 5, padding: 20 }}
+          onPress={() => { this.startTracking() }}
+        >
+          <Text style={{ color: '#fff' }}>Click here to start tracking</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
